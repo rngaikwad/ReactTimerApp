4f8ec9171f1826c42e948aadad213ebb10756735
rngaikwad <rnggaikwad@gmail.com>
2017-03-07 17:57:39 +0530

    finalize tests and add page title

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -64,6 +64,7 @@ var Controls = require('Controls');
  		};
  		return (
  			<div>
+ 				<h1 className="page-title">Countdown App</h1>
  				<Clock totalSeconds={count}/>
  				{renderControlArea()}
  			</div>
